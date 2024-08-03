6defd534c6eca1c127d13c97ed6abce158b157fe
PranayReddy-K <pranay.reddy12@outlook.com>
2024-08-03 12:54:05 +0530

    updated go version--->header updation ---->static files i.e images

--- main.go
+++ main.go
@@ -7,20 +7,24 @@ import (
 
 // Handler for serving Issues_Faced.html
 func issuesPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/Issues_Faced.html")
+	http.ServeFile(w, r, "static_files/Issues_Faced.html")
 }
 
 // Handler for serving Project_Info.html
 func projectInfoPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/Project_Info.html")
+	http.ServeFile(w, r, "static_files/Project_Info.html")
 }
 
 // Handler for serving Workflow_and_Tools.html
 func workflowAndToolsPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/Workflow_and_Tools.html")
+	http.ServeFile(w, r, "static_files/Workflow_and_Tools.html")
 }
 
 func main() {
+
+	// Serve static files from the "static_files" directory
+	fs := http.FileServer(http.Dir("static_files"))
+	http.Handle("/static_files/", http.StripPrefix("/static_files/", fs))
 	// Map URLs to their respective handler functions
 	http.HandleFunc("/issues", issuesPage)
 	http.HandleFunc("/project-info", projectInfoPage)
